af078e67f87e6011699384aaa4ec629a99246c28
Renjf <2142814970@qq.com>
2026-05-05 14:56:46 +0800

    chore: 移除 front/dist 构建产物，添加 .gitignore

--- front/dist/assets/index-DOV0f3Ro.js
+++
@@ -1,1 +0,0 @@
-const l=[{label:"待成交",value:0},{label:"已成交",value:1},{label:"已撤销",value:2}],a=[{label:"系统通知",value:1},{label:"交易提醒",value:2},{label:"风控通知",value:3},{label:"活动通知",value:4}];export{a as M,l as O};
