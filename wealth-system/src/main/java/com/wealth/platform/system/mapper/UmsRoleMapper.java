394982f5174b03ec8b42391b221950a9822c136f
Renjf <2142814970@qq.com>
2026-05-24 14:47:09 +0800

    chore: 删除旧微服务模块及遗留 seata 配置

--- wealth-system/src/main/java/com/wealth/platform/system/mapper/UmsRoleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.wealth.platform.system.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wealth.platform.system.entity.UmsRole;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UmsRoleMapper extends BaseMapper<UmsRole> {
-}
